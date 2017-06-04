3e2e6ad096b9d6fee788ad3cf3445157a63102c0
Alen Sendro <alen.sendro@gmail.com>
2017-06-04 18:47:44 +0200

    Clean things up

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,16 +1,35 @@
 import React from 'react';
+import Quiz from './../quiz';
+import Question from './../question';
+import Answer from './../answer';
+import './index.css';
+
+const answers = [
+  <Answer id="a" key="a" content="Answer 1" />,
+  <Answer id="b" key="b" content="Answer 2" />,
+  <Answer id="c" key="c" content="Answer 3" />
+];
+
+const questions = [
+  <Question id="1" key="1" content="What is callback?" answers={answers} />,
+  <Question id="2" key="2" content="What is closure?" answers={answers} />
+];
+
+const quiz = <Quiz
+    title="Advanced JS"
+    questions={questions}
+    numOfQuestions={questions.length}
+    category="Javascript"
+    description="Advanced questions from javascript" />
 
 class Home extends React.Component {
   render() {
-    let { list } = this.props;
     return (
       <div>
         <h2 style={{ textAlign: 'center' }}>Choose one quiz from list bellow</h2>
-        {list}
+        {quiz}
       </div>
-
     );
   }
 }
-
 export default Home;
--- src/index.js
+++ src/index.js
@@ -1,32 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import Home from './components/home';
-import Quiz from './components/quiz';
-import Question from './components/question';
-import Answer from './components/answer';
+import Home from './components/home';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
 // Root component
 const app = document.getElementById('app');
 
-const answers = [
-  <Answer id="a" key="a" content="Answer 1" />,
-  <Answer id="b" key="b" content="Answer 2" />,
-  <Answer id="c" key="c" content="Answer 3" />
-];
-
-const questions = [
-  <Question id="1" key="1" content="What is callback?" answers={answers} />,
-  <Question id="2" key="2" content="What is closure?" answers={answers} />
-];
-
-const quiz = <Quiz
-    title="Advanced JS"
-    questions={questions}
-    numOfQuestions={questions.length}
-    category="Javascript"
-    description="Advanced questions from javascript" />
-
-ReactDOM.render(quiz, app);
+ReactDOM.render(<Home />, app);
 registerServiceWorker();
